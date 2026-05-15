9f6590fb8c0588add078bbe432c9f6fa32be5570
davidCendejas712 <cendedavid@gmail.com>
2026-05-15 17:50:49 +0200

    Modelos mel entrenados

---
+++ setup/lanzar.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Lanzador generado automáticamente por instalar.sh
+source "/home/cendejas/miniconda3/etc/profile.d/conda.sh"
+conda activate audio_gpu
+cd "/home/cendejas/TFG-Inferencia-de-sonidos-musicales-usando-sintetizadores-e-IA/setup/../Prototipo5"
+python main.py
